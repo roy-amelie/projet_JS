0260722b4a6dd3306db320f6a328e474ce0d14dc
roy-amelie <roydorelon.amelie@gmail.com>
2020-04-01 14:08:57 +0200

    fonction affichage post

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 function fetchAllPosts() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch("https://ghibliapi.herokuapp.com/films")
         .then(response => response.json())
-        .then(json => json.forEach(element => {
-            console.log(element.title)
-            let new_div_element = document.createElement('div');
-            new_div_element.style.border="solid #6B2737";
-            new_div_element.style.borderRadius= "25px";
-            new_div_element.style.margin="20px";
-            new_div_element.style.padding="20px";
-            let post_title = document.createElement('h4');
-            post_title.textContent = element.title;
-            new_div_element.appendChild(post_title);
-            let new_p = document.createElement('p');
-            new_p.textContent = element.body;
-            new_div_element.appendChild(new_p);
-            parent_element=document.querySelector('.post');
-            parent_element.insertBefore(new_div_element,parent_element.firstChild)
-        }));
+        .then(post => {createFeed(post);})
+}
 
+function createFeed(data){
+    let post= document.querySelector('.post');
+    data.forEach(element=>{
+        post.appendChild(createPost(element));
+    })
+}
+
+function createPost(dataPost){
+    let new_div_element = document.createElement('div');
+    new_div_element.style.border = "solid #6B2737";
+    new_div_element.style.borderRadius = "25px";
+    new_div_element.style.margin = "20px";
+    new_div_element.style.padding = "20px";
+    let post_title = document.createElement('h4');
+    post_title.textContent = dataPost.title;
+    new_div_element.appendChild(post_title);
+    let new_p = document.createElement('p');
+    new_p.textContent = dataPost.description;
+    new_div_element.appendChild(new_p);
+    parent_element = document.querySelector('.post');
+    parent_element.insertBefore(new_div_element, parent_element.firstChild);
+    return new_div_element;
 }
 
 fetchAllPosts();
-//console.log(post)
 
-
-
-let new_div_element = document.createElement('div');
-let post_title = document.createElement('h4');
-post_title.textContent = post[i].title;
-new_div_element.appendChild(post_title);
-let new_p = document.createElement('p');
-new_p.textContent = post[i].body;
-new_div_element.appendChild(new_p);
 
